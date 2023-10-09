8de649c9f0d71043d149ecfe0fe10950c6c12fd3
Pinoyburn <deondre.garcia@yahoo.com>
2023-10-09 15:01:13 -0700

    minor Landing changes

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,44 @@
 import React, { useState, useEffect } from "react";
 import SoCalCountiesSVG from "../assets/svg/SoCalCountiesSVG";
-import CountyOverview from "../components/CountyOverview/CountyOverview";
 import Carousel from "../components/Carousel/Carousel";
+import { ImageDir } from "../assets/images/ImageDirectory";
 import "./Landing.css";
+
+const countyNameArray = [
+  "Kern",
+  "San Luis Obispo",
+  "Santa Barbara",
+  "Ventura",
+  "Los Angeles",
+  "San Bernardino",
+  "Riverside",
+  "Orange",
+  "Imperial",
+  "San Diego",
+  "Tap on a county",
+];
+
+const countyStringIDArray = [
+  "Kern",
+  "SanLuis",
+  "SanBar",
+  "Ven",
+  "LosAng",
+  "SanBern",
+  "River",
+  "Orange",
+  "Imperial",
+  "SanDiego",
+  "Tap on a county",
+];
 
 const Landing = () => {
   // each of 10 counties will correspond to 0-9, while 10 means none are hovered
   const [hoverIndex, setHoverIndex] = useState("10");
 
   const [indexCounter, setIndexCounter] = useState(0);
+
+  const countyIndex = Number(hoverIndex);
 
   useEffect(() => {
     let timer = setTimeout(() => {
@@ -22,7 +52,27 @@ const Landing = () => {
     <div className="landing-container">
       <h1>SoCal Unveiled</h1>
       <div className="landing">
-        <CountyOverview hoverIndex={hoverIndex} />
+        <div className="county-overview-container">
+          <h1>{countyNameArray[countyIndex]}</h1>
+          <div className="overview-images-container">
+            {ImageDir[countyStringIDArray[countyIndex]]?.map((image, index) => {
+              return (
+                <img
+                  className="overview-image"
+                  src={image}
+                  key={index}
+                  alt={`${countyNameArray[countyIndex]} County`}
+                />
+              );
+            })}
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            quaerat quam nesciunt a debitis harum ipsa sed? Adipisci soluta vel,
+            accusantium, dolorem nesciunt qui nostrum, id officia laboriosam
+            architecto similique.
+          </p>
+        </div>
         <div className="map-container">
           <SoCalCountiesSVG
             hoverIndex={hoverIndex}
@@ -32,13 +82,13 @@ const Landing = () => {
         </div>
       </div>
       <Carousel countyName="LosAng" />
+      <Carousel countyName="SanDiego" />
+      <Carousel countyName="Orange" />
       <Carousel countyName="SanBern" />
       <Carousel countyName="Kern" />
       <Carousel countyName="SanLuis" />
       <Carousel countyName="Ven" />
-      <Carousel countyName="SanDiego" />
       <Carousel countyName="River" />
-      <Carousel countyName="Orange" />
       <Carousel countyName="Imperial" />
       <Carousel countyName="SanBar" />
     </div>
